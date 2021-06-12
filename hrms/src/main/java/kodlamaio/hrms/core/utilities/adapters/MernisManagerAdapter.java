8b1b25f215b039d6030d82170c1cb93f241d1826
nurgulkaya <nnurgulky@gmail.com>
2021-06-13 01:21:28 +0300

    add project

---
+++ hrms/src/main/java/kodlamaio/hrms/core/utilities/adapters/MernisManagerAdapter.java
@@ -0,0 +1,10 @@
+package kodlamaio.hrms.core.utilities.adapters;
+
+public class MernisManagerAdapter implements MernisService {
+
+	@Override
+	public boolean mernisServiceCheck() {
+		return true;
+	}
+
+}
